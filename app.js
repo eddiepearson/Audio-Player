9bb1037f485d7bc9b27574588a2ea58b8cc052d8
eddie pearson <eddieismusic@gmail.com>
2020-05-14 08:12:06 -0400

    small changes to html and js

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
 const songTitle = document.querySelector('.song-title');
 const artistName = document.querySelector('.artist-name');
+const track1 = document.querySelector('#song1');
+const track2 = document.querySelector('#song2');
+const track3 = document.querySelector('#song3');
+const playButton = document.querySelector('.play');
 
 const songs = {
     song1: {
         title: 'Automation City',
         artist: 'eddiepearson',
-        url: 'https://www.dl.dropboxusercontent.com/s/qpo88jv7b9a2wk8/Automation%20city%20master.mp3?dl=0',
+        audio: track1,
     },
 
     song2: {
         title: 'Changing It',
         artist: 'eddiepearson',
-        url: 'https://www.dl.dropboxusercontent.com/s/zv34txare3ixvpl/Changing%20it%20master.mp3?dl=0',
+        audio: track2,
     },
 
     song3: {
         title: 'Finding Her',
         artist: 'eddiepearson',
-        url: 'https://www.dl.dropboxusercontent.com/s/13wyh0xxo9ts9g2/finding%20her%20soundtrack.mp3?dl=0',
+        audio: track3,
     },
 };
 
-// function to Select the song title and change it to the song title in the object
-function changeText(event) {
-
-};
+playButton.addEventListener('click', function () {
+    track1.play();
+});
